7a71ff462ff41c706cf8e59cd76fbe0735f30f6d
Edmund Dipple <elmundio1987@gmail.com>
2011-04-23 19:58:48 +0100

    ensure that the HTTP headers match that of a CSS file

--- CSS3Bundler.php
+++ CSS3Bundler.php
@@ -1,14 +1,12 @@
 <?php
+header('Content-type: text/css');
+$days_to_cache = 10;
+header('Expires: '.gmdate('D, d M Y H:i:s',time() + (60 * 60 * 24 * $days_to_cache)).' GMT');
 
 	$file = file_get_contents('exampleCSS.css');
 
-
-
-
-
 while(strpos($file, "@CSS3_GRADIENT") != false)
 {	
-
 	$rule = "";
 	$firstcolour = "";
 	$secondcolour = "";
